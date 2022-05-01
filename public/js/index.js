57881644aac340dfd5e548bc981739ca5c162f23
renzo <ralvarezrenzo@gmail.com>
2022-05-01 16:46:36 -0500

    add color lila elements header and bg header

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,8 @@
 import menuOpen from "./modules/menu.js";
+import escritura from "./modules/escritura.js";
 
 const d = document;
-
 d.addEventListener("DOMContentLoaded",e=>{
     menuOpen(".btn-menu",".menu",".header")
+    escritura();
 })
---
+++ public/js/modules/escritura.js
@@ -0,0 +1,17 @@
+export default function escritura(){
+    const typed = new Typed('.typed',{
+        strings : ['Front End Developer'],
+        stringsElement: null,
+        typeSpeed: 100,
+        startDelay: 1250,
+        backSpeed: 100,
+        smartBackspace: true,
+        shuffle: false,
+        backDelay: 3000,
+        loop: true,
+        loopCount: false,
+        showCursor: true,
+        cursorChar: '|',
+        contentType: 'html',
+    });
+}
